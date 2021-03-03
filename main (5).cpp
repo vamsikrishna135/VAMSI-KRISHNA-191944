7587c5006fee3619d07276826eb71f6b1825d156
vamsikrishna135 <79443794+vamsikrishna135@users.noreply.github.com>
2021-03-03 16:07:27 +0530

    Add files via upload

---
+++ main (5).cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    double m;
+    int n=2;
+    
+    cout<<" Lab Assignment of Power Function \n";
+    
+    cout<<"Enter the value of m\n";
+    cin>>m;
+    
+    cout<<m<<"^"<<n<<"="<<pow(m,2);
+    
+    return 0;
+    
+}
